768420960854be4cd4257e190f13c94667e8830d
Jetter <jlgarcia2@student.apc.edu.ph>
2025-08-23 13:25:40 +0800

    chore: setup jest

---
+++ jest.setup.ts
@@ -0,0 +1,17 @@
+/**
+ * Jest setup file
+ * This file is executed once before all tests
+ */
+
+import "@testing-library/jest-dom";
+
+// Global test setup can go here
+// Optionally suppress console logs during tests unless needed for debugging
+// global.console = {
+//   ...console,
+//   log: jest.fn(),
+//   debug: jest.fn(),
+//   info: jest.fn(),
+//   warn: jest.fn(),
+//   error: jest.fn(),
+// };
